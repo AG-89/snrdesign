85c90843ebfb42a3369d2318efdb547d8bc16ef9
Brandon G Yager <bgyager@pnw.edu>
2018-09-18 17:21:16 -0500

    master commit test

--- autotune_matlabprac.m
+++ autotune_matlabprac.m
@@ -4,6 +4,8 @@ clc, clear variables, close all, clear sound, clear sounds;
 
 global count; count = 420; %temp for quadratic interp graphs
 
+%mast commit
+
 %CONTROL VARIABLES
     %constants
     samplerate = 44100; %sample rate
